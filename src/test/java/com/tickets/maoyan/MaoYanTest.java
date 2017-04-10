ea0de10c3f1ed7186ce460372d9448cac63269db
feiyu <1119290647@qq.com>
2017-04-10 17:57:06 +0800

    添加测试类

--- src/test/java/com/tickets/maoyan/MaoYanTest.java
+++ src/test/java/com/tickets/maoyan/MaoYanTest.java
@@ -10,9 +10,10 @@ import com.util.UrlConstant;
 
 public class MaoYanTest
 {
-//    @Test
+    @Test
     public void testCity()
     {
+        // id=42 西安
         String string = HttpUtils.get(UrlConstant.MAOYAN_CITY_URL, "");
         System.out.println(string);
         JSONObject resultJson = (JSONObject)JSON.parse(string);
@@ -29,6 +30,7 @@ public class MaoYanTest
     
     @Test
     public void testCinemas(){
+        // id=867
         String cinemas = HttpUtils.get(UrlConstant.MAOYAN_LOCAL_CINEMAS);
         JSONObject data = JSON.parseObject(cinemas).getJSONObject("data");
         for(String key : data.keySet()){
@@ -39,7 +41,30 @@ public class MaoYanTest
     
     @Test
     public void testHotMovies(){
-        String hotMovies = HttpUtils.post("http://m.maoyan.com/movie/list.json", "type=hot&offset=0&limit=100");
+        // id = 246969
+        String hotMovies = HttpUtils.get("http://m.maoyan.com/movie/list.json", "type=hot&offset=0&limit=100");
         System.out.println(hotMovies);
+    }
+    
+    @Test
+    public void testMovieCommit(){
+        String reqUrl = UrlConstant.MAOYAN_MOVIE_COMMENT.replace("{movieId}", "246969");
+        String commits = HttpUtils.get(reqUrl);
+        System.out.println(commits);
+    }
+    
+    @Test
+    public void TestCinemaInfo(){
+        String reqUrl = UrlConstant.MAOYAN_CINEMAL_DETAIL.replace("{cinemaId}", "867").replace("{movieId}", "246969");
+        String commits = HttpUtils.get(reqUrl);
+        System.out.println(commits);
+    }
+    
+    @Test
+    public void TestSeat(){
+        String reqUrl = UrlConstant.MAOYAN_SEAT_INFO.replace("{showId}", "149068").replace("{showDate}", "20170411");
+        System.out.println(reqUrl);
+        String commits = HttpUtils.get(reqUrl);
+        System.out.println(commits);
     }
 }
